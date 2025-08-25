048d9033f545151531a2ced7a9699dfba28c836b
MingulovTA <mingulov.t@yandex.ru>
2025-08-25 17:30:16 +0700

    feat: entity self initialization added

--- src/quiver-survivor-entitas/Assets/Code/Infrastructure/View/EntityBehaviour.cs
+++ src/quiver-survivor-entitas/Assets/Code/Infrastructure/View/EntityBehaviour.cs
@@ -1,13 +1,22 @@
-﻿using Code.Infrastructure.View.Registrars;
+﻿using Code.Gameplay.Common.Collisions;
+using Code.Infrastructure.View.Registrars;
 using UnityEngine;
+using Zenject;
 
 namespace Code.Infrastructure.View
 {
     public class EntityBehaviour : MonoBehaviour, IEntityView
     {
+        private ICollisionRegistry _collisionRegistry;
         private GameEntity _gameEntity;
 
         public GameEntity GameEntity => _gameEntity;
+        
+        [Inject]
+        private void Construct(ICollisionRegistry collisionRegistry)
+        {
+            _collisionRegistry = collisionRegistry;
+        }
 
         public void SetEntity(GameEntity gameEntity)
         {
@@ -17,12 +26,19 @@ namespace Code.Infrastructure.View
             
             foreach (var registrar in GetComponentsInChildren<IEntityComponentRegistrar>())
                 registrar.RegistrerComponents();
+
+
+            foreach (var collider2D in GetComponentsInChildren<Collider2D>(true))
+                _collisionRegistry.Register(collider2D.GetInstanceID(),_gameEntity);
         }
 
         public void ReleaseEntity()
         {
             foreach (var registrar in GetComponentsInChildren<IEntityComponentRegistrar>())
                 registrar.UnregistrerComponents();
+            
+            foreach (var collider2D in GetComponentsInChildren<Collider2D>(true))
+                _collisionRegistry.Unregister(collider2D.GetInstanceID());
             
             _gameEntity.Release(this);
             _gameEntity = null;
---
+++ src/quiver-survivor-entitas/Assets/Code/Infrastructure/View/SelfInitializedEntityView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Code.Common.Entity;
+using Code.Infrastructure.Identifiers;
+using UnityEngine;
+using Zenject;
+
+namespace Code.Infrastructure.View
+{
+    public class SelfInitializedEntityView : MonoBehaviour
+    {
+        [SerializeField] private EntityBehaviour _entityBehaviour;
+
+        private IIdentifierService _identifierService;
+        [Inject]
+        private void Construct(IIdentifierService identifierService)
+        {
+            _identifierService = identifierService;
+        }
+        private void Awake()
+        {
+            GameEntity gameEntity = CreateEntity
+                .Empty()
+                .AddId(_identifierService.Next());
+            
+            _entityBehaviour.SetEntity(gameEntity);
+        }
+    }
+}
